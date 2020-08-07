fadfd920194e5725af8930f0cade98d2fd29e91c
lenin nog <leninnogueira@gmail.com>
2020-08-07 22:15:25 +0000

    add help command, remove dcnt and add 'suggestion:' as an alias to 'request'

--- main.py
+++ main.py
@@ -22,13 +22,32 @@ class byterbot(discord.Client):
         if m.content.startswith('%'):
             ctx = m.content[1:].split(' ')
             cm = ctx[0]
-            if cm == "t":
+
+            if cm == "help":
+                embed=discord.Embed(color=0x301baa)
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/740003037141008504/740020475245232219/BUTTON_byter.webp")
+                embed.add_field(name="Hello! here are the valid commands",
+                                value='''
+                                        **help** - show this info
+                                        **gifs** - lists all loaded categories of gifs/images
+
+                                      ''',
+                                inline=False
+                                )
+                embed.add_field(name="And functions",
+                                value='''
+                                        **request: / suggestion:** - a message starting with these will be added and up and down arrow reaction, for voting
+                                        **gifs / images !** - use the command %gifs to see what categories are avaiable and use % plus the name for me to pick a gif/image for you!
+                                        
+                                      ''',
+                                inline=False
+                                )
+                embed.set_footer(text="© creucat.com - developed by leninnog")
+                await m.channel.send('', embed=embed)
+
+            elif cm == "t":
                 await m.channel.send('Bot online')
                 print(m.content)
-
-            elif cm == "dcnt" and m.author.id == 310449948011528192:
-                await m.channel.send('Shutting down...')
-                await self.close()
 
             elif cm == "gifs":
                 await m.channel.send(
@@ -49,15 +68,16 @@ class byterbot(discord.Client):
                 self.reDb[m.content] = [m.attachments[0].url]
             print("[Bot - Info]: (l) new gif detected, added to db, now %s categories loaded" % len(self.reDb))
 
-        elif m.content.startswith('request:'):
+        elif m.content.startswith(('request:', 'suggestion:')):
             await m.add_reaction("⬆")
             await m.add_reaction("⬇")
 
-        elif "good night" in m.content.lower() or "goodnight" in m.content.lower():
-            await m.add_reaction("❤️")
-        
         elif "virus" in m.content.lower():
             await m.add_reaction(self.get_emoji(726611950200553502))
+
+        elif "good night" in m.content.lower() or "goodnight" in m.content.lower():
+            await m.add_reaction("❤️")
+
 
 ka.ka()
 ka.status = "starting"
