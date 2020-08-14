554ae21a27a8feb75e5a238bf57f90b5b180d041
leninnog <leninnogueira@gmail.com>
2020-08-14 17:43:43 -0300

    add more info to %info and %help

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ class byterbot(discord.Client):
     readyTime = 0
     loadTime = 0
 
-    characters = open('data/characters.json')
-    characters = json.load(characters)
+    charFile = open('data/characters.json')
+    characters = json.load(charFile)
+    charFile.close()
 
     async def on_ready(self):
-        self.readyTime = time() 
-        
+        self.readyTime = time()         
         async for i in self.get_channel(740539699134857337).history():
             if i.content in self.reDb:
                 self.reDb[i.content].append(i.attachments[0].url)
@@ -31,44 +31,47 @@ class byterbot(discord.Client):
             cm = ctx[0]
 
             if cm == "help":
-                embed=discord.Embed(color=0x301baa)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/741457274530299954/741457798705184830/BUTTON_byter.webp")
-                embed.add_field(name="Hello! here are the valid commands",
-                                value='''
-                                        **help** - show this info about commands
-                                        **info** - shows many infos about créu and his friends!
-                                        **gifs** - lists all loaded categories of gifs/images
-                                        **stats** - shows some useful stats
-                                        **poll title, *options** - makes a poll, options may be none (yes/no) or phrases separated by a comma (up to 20), title may also be omitted if there arent other arguments
-                                      ''',
-                                inline=False)
-                embed.add_field(name="And functions",
-                                value='''
-                                        **gifs / images !** - use the command %gifs to see what categories are avaiable and use % plus the name for me to pick a gif/image for you!
-                                      ''',
-                                inline=False)
+                embed=discord.Embed(color=0x301baa,
+                                    title="**Hello! here are the valid commands**",
+                                    description='''
+                                            **help** - show this info about commands
+                                            **info** - shows many infos about créu and his friends!
+                                            **gifs** - lists all loaded categories of gifs/images
+                                            **stats** - shows some useful stats
+                                            **poll title, *options** - makes a poll, options may be none (yes/no) or phrases separated by a comma (up to 20), title may also be omitted if there arent other arguments
+                                          ''')
                 embed.add_field(name="Also, here is a copyright disclaimer!",
-                                value='''
-                                        © 2020 PriVer. All rights reserved. All visual content on this bot (including characters, images and trademarks) are protected by Intellectual Property rights owned by Priscila Vertamatti.
-                                      ''',
+                                value="© 2020 PriVer. All rights reserved. All visual content on this bot (including characters, images and trademarks) are protected by Intellectual Property rights owned by Priscila Vertamatti.",
                                 inline=False)
-                embed.set_footer(text="creucat.com © PriVer - bot developed by leninnog",
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/741457274530299954/741457798705184830/BUTTON_byter.webp")
+                embed.set_footer(text="[creucat.com](https://creucat.com) © PriVer - bot developed by leninnog - [want me in your server? click here!](https://discord.com/api/oauth2/authorize?client_id=740006457805635678&permissions=537259072&scope=bot)",
                                  icon_url="https://cdn.discordapp.com/attachments/741457274530299954/741457487277850724/creucat.ico.gif")
                 await m.channel.send('', embed=embed)
 
             elif cm == "info":
+                if len(ctx) == 1:
+                    embed = discord.Embed(color=0x301baa,
+                                          title="**Info!**",
+                                          value='''
+                                                  Currently there's info only for characters!
+                                                  use `char` or `character` after this command
+                                                ''')
+                    embed.set_thumbnail(url=choice(["https://cdn.discordapp.com/attachments/741457274530299954/741615794340888586/selocreu2.gif", "https://cdn.discordapp.com/attachments/741457274530299954/741616136134852678/selocreu1.gif"]))
+                    embed.set_footer(text="[creucat.com](https://creucat.com) © PriVer - bot developed by leninnog",
+                                     icon_url="https://cdn.discordapp.com/attachments/741457274530299954/741457487277850724/creucat.ico.gif")
+
                 if ctx[1] in ["character", "char"]:
                     if len(ctx) == 2:
-                        embed = discord.Embed(color=0x00002a)
-                        embed.add_field(name="Characters!",
-                                        value='''
+                        embed = discord.Embed(color=0x00002a,
+                                              title="**Characters!**",
+                                              description='''
                                                 Want to know about the créu characters? this is the way to go!
 
                                                 Just put the name of the character you want to know in front of this command! they are Créu, Petita, Liu-Liu, Muji, Printy, Mek & Krek, Rona & mou and of course me!
-                                              ''',
-                                        inline=False)
+                                              '''
+                                            )
                         embed.set_thumbnail(url=choice(["https://cdn.discordapp.com/attachments/741457274530299954/741615794340888586/selocreu2.gif", "https://cdn.discordapp.com/attachments/741457274530299954/741616136134852678/selocreu1.gif"]))
-                        embed.set_footer(text="creucat.com/characters © PriVer - bot developed by leninnog",
+                        embed.set_footer(text="[creucat.com/characters](https://creucat.com/characters) © PriVer - bot developed by leninnog",
                                          icon_url="https://cdn.discordapp.com/attachments/741457274530299954/741457487277850724/creucat.ico.gif")
                         await m.channel.send('', embed=embed)
                         return 1
@@ -91,7 +94,7 @@ class byterbot(discord.Client):
                     embed.add_field(name=charData['name'], value=charData['desc'], inline=False)
                     embed.add_field(name="Favorites", value="<:coffee:741469635492446268> %s\n\n<:ice_cream:741469513773613118> %s\n\n<:music:741469877143076946> %s" % tuple(charData['favs']), inline=False)
                     embed.set_image(url=charData['img'])
-                    embed.set_footer(text="creucat.com/characters © PriVer - bot developed by leninnog",
+                    embed.set_footer(text="[creucat.com/characters](https://creucat.com/characters) © PriVer - bot developed by leninnog",
                                         icon_url="https://cdn.discordapp.com/attachments/741457274530299954/741457487277850724/creucat.ico.gif")
 
                 await m.channel.send('', embed=embed)
@@ -109,11 +112,11 @@ class byterbot(discord.Client):
 
             elif cm == "stats":
                 embed = discord.Embed(color=0x301baa)
-                embed.add_field(name="Here are some time metrics",
+                embed.add_field(name="Here are some numbers I found",
                                 value='''
                                         **uptime**: I'm online for %s (%s seconds)
                                         **ready time**: I took %s seconds to connect to discord
-                                        **load time**: I took %s seconds to load after connecting
+                                        **loading time**: I took %s seconds to load after connecting
                                       '''
                                       % (
                                          timedelta(seconds=round(time()-initTime)),
@@ -124,7 +127,7 @@ class byterbot(discord.Client):
                                 inline=False)
                 await m.channel.send('', embed=embed)
 
-            elif cm == "poll":    
+            elif cm == "poll":
                 embed = discord.Embed(color=0xb20ac5)
                 options = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷' ,'🇸', '🇹']
                 poll = m.content.replace('b!', '%')[5:].split(',')
