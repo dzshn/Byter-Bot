1af6b7a8a02dbc1af4213140770bc47e069aee24
dzshn <zshn@pm.me>
2021-01-12 08:26:26 -0300

    Add a configurable reaction system

--- exts/creu_server.py
+++ exts/creu_server.py
@@ -1,34 +1,47 @@
+import json
+
 import discord
 from discord.ext import commands
+
 
 class CreuServerStuff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.cguild = self.bot.get_guild(725421276562325514)
+        try:
+            self.reactions = json.load(open('reactions.json'))
+
+        except FileNotFoundError:
+            self.reactions = []
+            open('reactions.json', 'w')
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.cguild = self.bot.get_guild(725421276562325514)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
+    async def do_reactions(self, message):
         if not message.author.bot and self.bot.get_user(self.bot.owner_id) in message.mentions:
             await message.add_reaction(self.bot.get_emoji(748824813501546559))
 
         if message.guild == self.cguild:
-            lowered = ''.join(i for i in message.clean_content.lower() if i in 'abcdefghijklmnopqrstuvwxyz')
+            lowered = ''.join(i for i in message.clean_content.lower() if i in 'abcdefghijklmnopqrstuvwxyz ').strip()
             if lowered == 'p':
                 await message.delete()
                 return
 
-            if "good night" in lowered or "goodnight" in lowered or "qnight" in lowered:
-                await message.add_reaction("❤️")
+            for obj in self.reactions:
+                for trigger in obj['triggers']:
+                    if trigger in lowered:
+                        await message.add_reaction(self.bot.get_emoji(obj['reaction']))
+                        break
 
-            if "dreamworks" in lowered or "poop" in lowered:
-                await message.add_reaction(self.bot.get_emoji(726611950200553502))
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        await self.do_reactions(message)
 
-            if "alastor" in lowered:
-                await message.add_reaction(self.bot.get_emoji(779946643859832833))
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        await self.do_reactions(after)
 
     @commands.command(hidden=True)
     async def mewhen(self, ctx):
